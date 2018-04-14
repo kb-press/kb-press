5202e176e0859d03c3c6ae27d2b7be0d6869a14a
Kenneth Lyons <ixjlyons@gmail.com>
2018-04-14 12:10:59 -0700

    Initial commit.

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+nbdir=notebooks
+builddir=_build
+
+if [ ! -d $builddir ]; then
+    mkdir $builddir
+fi
+
+for f in $nbdir/*.ipynb; do
+    # execute notebook and export to HTML
+    jupyter nbconvert "$f" --execute --to html --output-dir $builddir
+done
+
+for f in $builddir/*.html; do
+    # replace links to local ipynb files with links to html files
+    sed -i 's/.ipynb/.html/g' "$f"
+done
